a2d85910d90d455db2f5a45ee7c8fb7c932dfe73
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-06-12 06:53:31 +0530

    react frontend dev

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>Welcome to the React application!</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+    return (
+        <header>
+        <h1>Welcome to My Website</h1>
+        <nav>
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#blog">Blog</a></li>
+            </ul>
+        </nav>
+        </header>
+    );
+}
